f7626bca89fc6d884b5b9271a32076a698930ceb
MonicaDeBona <debona.monica@outlook.it>
2023-03-27 17:29:55 +0200

    trying address filter

--- src/components/SearchComponent.vue
+++ src/components/SearchComponent.vue
@@ -1,9 +1,16 @@
 
 <script>
+import { store } from '../store'
 export default {
-    name: "SearchComponent"
+    name: "SearchComponent",
+    data() {
+        return {
+            store,
+        }
+    },
 
 }
+
 </script>
 
 <template lang="">
@@ -12,13 +19,14 @@ export default {
                 <div class="col-12">
                     <form @submit.prevent="searchApartments">
                         <div class="input-group">
-                            <input v-model="searchQuery" type="text" class="form-control"
+                            <input v-model="store.searchQuery" type="text" class="form-control"
                                 placeholder="Search for an address or city" />
                             <!-- <button type="submit" class="btn btn-primary">Search</button> -->
-                            <router-link :to="{ name: 'apartments' }" class="btn btn-primary" type="submit"
+                            <router-link :to="{ name: 'apartments' }" class="btn btn-primary" type="submit" @click="$emit(store.searchQuery)"
                                 >
                                 Cerca
                             </router-link>
+                        
                         </div>
                     </form>
                 </div>
--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,49 +1,50 @@
 <script>
 import SearchComponent from '../components/SearchComponent.vue';
-import axios from "axios";
+// import axios from "axios";
+// import { store } from '../store.js'
 
 export default {
     name: 'HomePage',
     components: {
         SearchComponent
     },
-    data() {
-        return {
-            apartments: [],
-            loading: false,
-            urlAddress: "http://127.0.0.1:8000",
-            searchQuery: "",
-        };
-    },
-    methods: {
-        getApartments() {
-            axios
-                .get(this.urlAddress + "/api/apartments/", {})
-                .then((response) => {
-                    this.apartments = response.data.results;
-                })
-                .catch((error) => {
-                    console.warn(error);
-                });
-        },
-        searchApartments() {
-            axios
-                .get(this.urlAddress + "/api/apartments/", {
-                    params: {
-                        address: this.searchQuery,
-                    },
-                })
-                .then((response) => {
-                    this.apartments = response.data.results;
-                })
-                .catch((error) => {
-                    console.warn(error);
-                });
-        },
-    },
-    created() {
-        this.getApartments();
-    },
+    // data() {
+    //     return {
+    //         apartments: [],
+    //         loading: false,
+    //         urlAddress: "http://127.0.0.1:8000",
+    //         searchQuery: "",
+    //     };
+    // },
+    // methods: {
+    //     getApartments() {
+    //         axios
+    //             .get(this.urlAddress + "/api/apartments/", {})
+    //             .then((response) => {
+    //                 this.apartments = response.data.results;
+    //             })
+    //             .catch((error) => {
+    //                 console.warn(error);
+    //             });
+    //     },
+    //     searchApartments() {
+    //         axios
+    //             .get(this.urlAddress + "/api/apartments/", {
+    //                 params: {
+    //                     address: this.searchQuery,
+    //                 },
+    //             })
+    //             .then((response) => {
+    //                 this.apartments = response.data.results;
+    //             })
+    //             .catch((error) => {
+    //                 console.warn(error);
+    //             });
+    //     },
+    // },
+    // created() {
+    //     this.getApartments();
+    // },
 
 }
 </script>
