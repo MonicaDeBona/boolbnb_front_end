f96d8b64fb70fa6af60f6b5282a6cad3b6415b64
giuliaballestrero <giulia.ballestrero@gmail.com>
2023-03-27 22:17:47 +0200

    Fix apartment api filter

--- src/components/SearchComponent.vue
+++ src/components/SearchComponent.vue
@@ -1,13 +1,35 @@
 
 <script>
 import { store } from '../store'
+import axios from 'axios';
+
 export default {
     name: "SearchComponent",
     data() {
         return {
             store,
+            urlAddress: "http://127.0.0.1:8000",
         }
     },
+
+    // methods: {
+    //     searchApartments(searchQuery) {
+    //         console.log(searchQuery);
+    //         axios.get(this.urlAddress + "/api/apartments/", {
+    //                 params: {
+    //                     address: searchQuery
+    //                 },
+    //             })
+
+    //             .then((response) => {
+    //                 this.store.apartments = response.data.results;
+    //             })
+
+    //             .catch((error) => {
+    //                 console.warn(error);
+    //             });
+    //     },
+    // },
 
 }
 
@@ -22,9 +44,11 @@ export default {
                             <input v-model="store.searchQuery" type="text" class="form-control"
                                 placeholder="Search for an address or city" />
                             <!-- <button type="submit" class="btn btn-primary">Search</button> -->
-                            <router-link :to="{ name: 'apartments' }" class="btn btn-primary" type="submit" @click="$emit(store.searchQuery)"
-                                >
+                            <router-link :to="{ name: 'apartments' }" class="btn btn-primary" 
+                                type="submit" @click="$emit('searchApartments', store.searchQuery)">
+                                
                                 Cerca
+                            
                             </router-link>
                         
                         </div>
--- src/pages/ApartmentList.vue
+++ src/pages/ApartmentList.vue
@@ -1,78 +1,61 @@
 <script>
 import ApartmentComponent from "../components/ApartmentComponent.vue";
+import SearchComponent from "../components/SearchComponent.vue";
+import { store } from '../store';
+
 import axios from "axios";
-import { store } from '../store.js'
 
 export default {
     name: "ApartmentList",
     components: {
         ApartmentComponent,
+        SearchComponent
+
     },
+
     data() {
         return {
-            store,
-            // apartments: [],
             loading: false,
             urlAddress: "http://127.0.0.1:8000",
-            // searchQuery: "",
+            store,
         };
     },
+
     methods: {
-        getApartments() {
-            axios
-                .get(this.urlAddress + "/api/apartments/", {})
-                .then((response) => {
-                    this.store.apartments = response.data.results;
-                })
-                .catch((error) => {
-                    console.warn(error);
-                });
-        },
-        searchApartments(searchQuery) {
-            console.log(searchQuery)
-            axios
-                .get(this.urlAddress + "/api/apartments/", {
+        searchApartments() {
+            axios.get(this.urlAddress + "/api/apartments/", {
                     params: {
-                        address: searchQuery,
+                        address: store.searchQuery
                     },
-
                 })
+
                 .then((response) => {
                     this.store.apartments = response.data.results;
                 })
+
                 .catch((error) => {
                     console.warn(error);
                 });
-
         },
     },
+
     created() {
-        this.getApartments();
+        this.searchApartments();
     },
-};
+};;
+
 </script>
+
 <template>
     <section>
         <div class="container">
-            <!-- <div class="row mb-3">
-                                                                                        <div class="col-12">
-                                                                                            <form @submit.prevent="searchApartments">
-                                                                                                <div class="input-group">
-                                                                                                    <input v-model="searchQuery" type="text" class="form-control"
-                                                                                                        placeholder="Search for an address or city" />
-                                                                                                    <button type="submit" class="btn btn-primary">Search</button>
-                                                                                                </div>
-                                                                                            </form>
-                                                                                        </div>
-                                                                                    </div> -->
+            <SearchComponent @searchApartments="searchApartments"/>
 
-            <h1 class="pb-5 text-center pt-5">Apartments list:</h1>
-
+            <h1 class="pb-5 text-center pt-5">Apartments in {{ store.searchQuery.toUpperCase() }}</h1>
             <div class="card-wrapper">
                 <!--Qui andranno le card-->
-                <ApartmentComponent v-for="apartmentElement in store.apartments" :apartment="apartmentElement"
+                <ApartmentComponent v-for="apartmentElement in this.store.apartments" :apartment="apartmentElement"
                     :imagePath="urlAddress" />
-
             </div>
         </div>
     </section>
@@ -84,7 +67,6 @@ export default {
     justify-content: center;
     flex-wrap: wrap;
 }
-
 .single-card {
     width: calc(100% / 3 - 2rem);
     margin: 1rem;
@@ -95,7 +77,6 @@ export default {
     box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
 }
 </style>
-
 
 
 
