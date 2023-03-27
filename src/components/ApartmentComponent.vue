c498ae896283cee1601aac8127322bfeef2e5f96
MonicaDeBona <debona.monica@outlook.it>
2023-03-27 13:12:56 +0200

    Trying filtered search

--- src/components/ApartmentComponent.vue
+++ src/components/ApartmentComponent.vue
@@ -1,87 +1,90 @@
 <script>
-    export default {
-        name: 'ApartmentComponent',
-    
-        props: {
-            'apartment': {
-                type: Object,
-                required: true,
-            },
-            
-            'isShow': {
-                type: Boolean,
-                required: false,
-                default: false, 
-            },
+export default {
+    name: 'ApartmentComponent',
 
-            'imagePath': {
-                type: String,
-                required: true
-            },
+    props: {
+        'apartment': {
+            type: Object,
+            required: true,
+        },
 
-        data () {
-        return {
-        //
-        }
+        'isShow': {
+            type: Boolean,
+            required: false,
+            default: false,
+        },
+
+        'imagePath': {
+            type: String,
+            required: true
+        },
+
+        data() {
+            return {
+                //
+            }
         }
     }
 }
 </script>
 
 <template>
-      <div class="card text-center mb-3 single-card p-3">
-            <h2 class="card-title fw-bold pt-2">
-                {{ apartment.title }}
-            </h2>
+    <div class="card text-center mb-3 single-card p-3">
+        <h2 class="card-title fw-bold pt-2">
+            {{ apartment.title }}
+        </h2>
 
-            <div class="card-body m-3">
-                
-                <img v-if="!apartment.image.startsWith('uploads')" :src="imagePath+'/img/'+apartment.image" class="img-thumbnail" :alt="apartment.title">
-                <img v-else :src="imagePath+'/storage/'+apartment.image" class="img-thumbnail" :alt="apartment.title">
+        <div class="card-body m-3">
 
-                <p class="card-text pt-4 mb-4">
-                    {{ apartment.address }}
-                </p>
+            <img v-if="!apartment.image.startsWith('uploads')" :src="imagePath + '/img/' + apartment.image"
+                class="img-thumbnail" :alt="apartment.title">
+            <img v-else :src="imagePath + '/storage/' + apartment.image" class="img-thumbnail" :alt="apartment.title">
 
-                <p class="card-text pt-4 mb-4">
-                    {{ apartment.n_rooms }}
-                </p>
+            <p class="card-text pt-4 mb-4">
+                {{ apartment.address }}
+            </p>
 
-                <p class="card-text pt-4 mb-4">
-                    {{ apartment.n_beds }}
-                </p>
+            <p class="card-text pt-4 mb-4">
+                rooms: {{ apartment.n_rooms }}
+            </p>
 
-                <p class="card-text pt-4 mb-4">
-                    {{ apartment.n_bathrooms}}
-                </p>
+            <p class="card-text pt-4 mb-4">
+                beds: {{ apartment.n_beds }}
+            </p>
 
-                <p class="card-text pt-4 mb-4">
-                    {{ apartment.square_meters}}
-                </p>
+            <p class="card-text pt-4 mb-4">
+                {{ apartment.n_bathrooms }}
+            </p>
 
-                <span class="fw-bold me-2">
-                    <a class="btn btn-disabled rounded-pill fw-bold me-2 mb-1"  v-for="service in apartment.services">
-                        {{service.name}}
-                    </a>
-                </span>
-            </div>
-        </div>   
+            <p class="card-text pt-4 mb-4">
+                {{ apartment.square_meters }}
+            </p>
+
+            <span class="fw-bold me-2">
+                <a class="btn btn-disabled rounded-pill fw-bold me-2 mb-1" v-for="service in apartment.services">
+                    {{ service.name }}
+                </a>
+            </span>
+        </div>
+    </div>
 </template>
 
 <style lang="scss" scoped>
 .card {
     border-radius: 30px;
     box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
-        img {
-            border-radius: 30px;
-        }
+
+    img {
+        border-radius: 30px;
+    }
 }
+
 .card-footer {
     background-color: #aba9ee8d;
-    color: white!important;
+    color: white !important;
     font-weight: bold;
 }
+
 a {
     border: 1px solid black;
-}
-</style>
+}</style>
--- src/pages/ApartmentList.vue
+++ src/pages/ApartmentList.vue
@@ -2,12 +2,12 @@
 //importo il component dei progetti
 import ApartmentComponent from '../components/ApartmentComponent.vue'
 //importo axios
-    import axios from 'axios';
+import axios from 'axios';
 
 export default {
     name: 'ApartmentList',
 
-    components:{
+    components: {
         ApartmentComponent,
     },
 
@@ -15,46 +15,49 @@ export default {
         return {
             apartments: [],
             loading: false,
-            urlAddress: 'http://127.0.0.1:8000',
+            urlAddress: 'http://127.0.0.1:8000/api/apartments/',
+            n_beds: '',
+            n_rooms: '',
         }
     },
 
     methods: {
-        getApartments(){
-            axios.get(this.urlAddress + '/api/apartments/', {
+        getApartments() {
+            axios.get(this.urlAddress, {
                 params: {
-                    
+                    n_beds: this.n_beds,
+                    n_rooms: this.n_rooms,
                 }
             })
-            .then((response) => {
-
-                //console.log(response.data.results.data);
-                this.apartments = response.data.results;
-            })
-            .catch(function (error) {
-                console.warn(error);
-            });
-        }
+                .then((response) => {
+                    this.apartments = response.data.results;
+                })
+                .catch(function (error) {
+                    console.warn(error);
+                });
+        },
     },
+
     created() {
         this.getApartments();
-    },
+    }
 }
 </script>
 
 <template>
-
     <section>
         <div class="container">
 
             <h1 class="pb-5 text-center pt-5">Apartments list:</h1>
+            <form @submit.prevent="getApartments">
+                <input type="number" v-model="n_beds" placeholder="Numero di letti" min="0" />
+                <input type="number" v-model="n_rooms" placeholder="Numero di stanze" min="0" />
+                <button type="submit">Cerca</button>
+            </form>
 
             <div class="card-wrapper">
-            <!--Qui andranno le card-->
-            
-                <ApartmentComponent
-                    v-for="apartmentElement in apartments" :apartment="apartmentElement" :imagePath="urlAddress"
-                />
+                <ApartmentComponent v-for="apartmentElement in apartments" :key="apartmentElement.id"
+                    :apartment="apartmentElement" />
             </div>
 
         </div>
@@ -63,10 +66,11 @@ export default {
 
 <style lang="scss" scoped>
 .card-wrapper {
-  display: flex;
-  justify-content: center;
-  flex-wrap: wrap;
+    display: flex;
+    justify-content: center;
+    flex-wrap: wrap;
 }
+
 .single-card {
     width: calc(100% / 3 - 2rem);
     margin: 1rem;
@@ -77,3 +81,5 @@ export default {
     box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
 }
 </style>
+
+
