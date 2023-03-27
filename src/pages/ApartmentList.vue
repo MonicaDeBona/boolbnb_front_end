56e4e13f88097864ef5e5f300d880b8b7884b0f3
MonicaDeBona <debona.monica@outlook.it>
2023-03-27 15:49:30 +0200

    Implemented address search

--- src/pages/ApartmentList.vue
+++ src/pages/ApartmentList.vue
@@ -1,65 +1,74 @@
 <script>
-//importo il component dei progetti
-import ApartmentComponent from '../components/ApartmentComponent.vue'
-//importo axios
-import axios from 'axios';
+import ApartmentComponent from "../components/ApartmentComponent.vue";
+import axios from "axios";
 
 export default {
-    name: 'ApartmentList',
-
+    name: "ApartmentList",
     components: {
         ApartmentComponent,
     },
-
     data() {
         return {
             apartments: [],
             loading: false,
-            urlAddress: 'http://127.0.0.1:8000/api/apartments/',
-            n_beds: '',
-            n_rooms: '',
-        }
+            urlAddress: "http://127.0.0.1:8000",
+            searchQuery: "",
+        };
     },
-
     methods: {
         getApartments() {
-            axios.get(this.urlAddress, {
-                params: {
-                    n_beds: this.n_beds,
-                    n_rooms: this.n_rooms,
-                }
-            })
+            axios
+                .get(this.urlAddress + "/api/apartments/", {})
                 .then((response) => {
                     this.apartments = response.data.results;
                 })
-                .catch(function (error) {
+                .catch((error) => {
+                    console.warn(error);
+                });
+        },
+        searchApartments() {
+            axios
+                .get(this.urlAddress + "/api/apartments/", {
+                    params: {
+                        address: this.searchQuery,
+                    },
+                })
+                .then((response) => {
+                    this.apartments = response.data.results;
+                })
+                .catch((error) => {
                     console.warn(error);
                 });
         },
     },
-
     created() {
         this.getApartments();
-    }
-}
+    },
+};;
 </script>
-
 <template>
     <section>
         <div class="container">
-
-            <h1 class="pb-5 text-center pt-5">Apartments list:</h1>
-            <form @submit.prevent="getApartments">
-                <input type="number" v-model="n_beds" placeholder="Numero di letti" min="0" />
-                <input type="number" v-model="n_rooms" placeholder="Numero di stanze" min="0" />
-                <button type="submit">Cerca</button>
-            </form>
-
-            <div class="card-wrapper">
-                <ApartmentComponent v-for="apartmentElement in apartments" :key="apartmentElement.id"
-                    :apartment="apartmentElement" />
+            <div class="row mb-3">
+                <div class="col-12">
+                    <form @submit.prevent="searchApartments">
+                        <div class="input-group">
+                            <input v-model="searchQuery" type="text" class="form-control"
+                                placeholder="Search for an address or city" />
+                            <button type="submit" class="btn btn-primary">Search</button>
+                        </div>
+                    </form>
+                </div>
             </div>
 
+            <h1 class="pb-5 text-center pt-5">Apartments list:</h1>
+
+            <div class="card-wrapper">
+                <!--Qui andranno le card-->
+                <ApartmentComponent v-for="apartmentElement in apartments" :apartment="apartmentElement"
+                    :imagePath="urlAddress" />
+
+            </div>
         </div>
     </section>
 </template>
@@ -81,5 +90,8 @@ export default {
     box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px;
 }
 </style>
+
+
+
 
 
