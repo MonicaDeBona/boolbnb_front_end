dccbe41c427f62e93b32cf94db65542a152ab8ba
giuliaballestrero <giulia.ballestrero@gmail.com>
2023-03-28 21:20:00 +0200

    Import Fontawesome

--- src/main.js
+++ src/main.js
@@ -3,4 +3,20 @@ import { router } from './router';
 import App from './App.vue';
 import 'bootstrap/dist/js/bootstrap.js';
 
-createApp(App).use(router).mount('#app')
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faCheck, faList } from '@fortawesome/free-solid-svg-icons'
+
+
+/* add icons to the library */
+library.add(faCheck, faList)
+
+createApp(App)
+.use(router)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount('#app')
