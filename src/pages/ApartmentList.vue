d53750e562a509b43fb12eb81bd7bcd3a092ff64
AngeloTripodi <tripodi.angelo@gmail.com>
2023-03-31 15:16:27 +0200

    fix container bug

--- src/pages/ApartmentList.vue
+++ src/pages/ApartmentList.vue
@@ -110,58 +110,61 @@ export default {
 
             </div>
 
-        </div>
 
-        <!-- Modal -->
-        <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
-            aria-labelledby="staticBackdropLabel" aria-hidden="true">
-            <div class="modal-dialog">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <h1 class="modal-title fs-5" id="staticBackdropLabel">Filters</h1>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body">
-                        <div class="row justify-content-between align-items-center mb-3">
-                            <div class="col-md-6 col-lg-4">
-                                <label class="form-label">Numero di letti</label>
-                                <input v-model.number="store.filters.n_beds" type="number" min="1" class="form-control">
+
+            <!-- Modal -->
+            <div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1"
+                aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <h1 class="modal-title fs-5" id="staticBackdropLabel">Filters</h1>
+                            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                        </div>
+                        <div class="modal-body">
+                            <div class="row justify-content-between align-items-center mb-3">
+                                <div class="col-md-6 col-lg-4">
+                                    <label class="form-label">Numero di letti</label>
+                                    <input v-model.number="store.filters.n_beds" type="number" min="1" class="form-control">
+                                </div>
+                                <div class="col-md-6 col-lg-4">
+                                    <label class="form-label">Numero di stanze</label>
+                                    <input v-model.number="store.filters.n_rooms" type="number" min="1"
+                                        class="form-control">
+                                </div>
                             </div>
-                            <div class="col-md-6 col-lg-4">
-                                <label class="form-label">Numero di stanze</label>
-                                <input v-model.number="store.filters.n_rooms" type="number" min="1" class="form-control">
+                            <div class="form-check" v-for="service in this.store.serviceList" :key="service.id">
+                                <input class="form-check-input" type="checkbox" :value="service.slug"
+                                    v-model="selectedServices" />
+
+                                <label class="form-check-label">{{ service.name }}</label>
                             </div>
                         </div>
-                        <div class="form-check" v-for="service in this.store.serviceList" :key="service.id">
-                            <input class="form-check-input" type="checkbox" :value="service.slug"
-                                v-model="selectedServices" />
 
-                            <label class="form-check-label">{{ service.name }}</label>
+                        <div class="modal-footer">
+                            <button type="button" class="btn my_btn" @click="searchApartments"
+                                data-bs-dismiss="modal">Search</button>
                         </div>
-                    </div>
-
-                    <div class="modal-footer">
-                        <button type="button" class="btn my_btn" @click="searchApartments"
-                            data-bs-dismiss="modal">Search</button>
                     </div>
                 </div>
             </div>
-        </div>
 
-        <!-- <h1 class="text-center py-3">Apartments in {{ store.searchQuery.toUpperCase() }}</h1> -->
-        <div
-            class="row align-items-center justify-content-center justify-content-sm-start d-flex align-items-stretch flex-wrap">
-            <ApartmentComponent v-for="apartmentElement in this.store.apartments" :apartment="apartmentElement"
-                :imagePath="urlAddress" />
-            <!--Qui andranno le card-->
-            <!--Messaggio da mostrare all'utente se non viene trovato nessun risultato-->
-            <div v-if="this.store.apartments.length === 0"
-                class="d-flex flex-column align-items-center justify-content-center py-5">
-                <img src="https://i.pinimg.com/564x/41/33/fc/4133fc74007d45c442cb41f0aeb6d919.jpg" alt="placeholeder-image"
-                    class="w-25 align-content-center">
-                <h5 class="text-center py-2">Sorry, no apartments found.</h5>
+
+            <!-- <h1 class="text-center py-3">Apartments in {{ store.searchQuery.toUpperCase() }}</h1> -->
+            <div
+                class="row align-items-center justify-content-center justify-content-sm-start d-flex align-items-stretch flex-wrap">
+                <ApartmentComponent v-for="apartmentElement in this.store.apartments" :apartment="apartmentElement"
+                    :imagePath="urlAddress" />
+                <!--Qui andranno le card-->
+                <!--Messaggio da mostrare all'utente se non viene trovato nessun risultato-->
+                <div v-if="this.store.apartments.length === 0"
+                    class="d-flex flex-column align-items-center justify-content-center py-5">
+                    <img src="https://i.pinimg.com/564x/41/33/fc/4133fc74007d45c442cb41f0aeb6d919.jpg"
+                        alt="placeholeder-image" class="w-25 align-content-center">
+                    <h5 class="text-center py-2">Sorry, no apartments found.</h5>
+                </div>
+
             </div>
-
         </div>
     </section>
 </template>
